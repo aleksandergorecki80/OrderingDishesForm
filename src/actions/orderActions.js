f8c9168c9085aa087384b1df07ed03fdb0f09955
Aleksander <aleksandergorecki80@gmail.com>
2021-11-28 06:36:34 +0000

    Make a request

--- src/actions/orderActions.js
+++ src/actions/orderActions.js
@@ -1,18 +1,29 @@
+// import axios from 'axios';
 
-// JSON Headers
-const config = {
-    headers: {
-        'Content-Type': 'application/json'
-    }
-}
-export const submitNewOrder = (formData) => {
-    const body = JSON.stringify(formData);
-    console.log(body, 'body')
-    return async (dispatch) => {
-        try {
-            console.log(formData)
-        } catch (err) {
-            console.log('error')
-        }
-    }
-}
+// const https = 'https://frosty-wood-6558.getsandbox.com/dishes';
+
+// // JSON Headers
+// const config = {
+//     headers: {
+//         'Content-Type': 'application/json'
+//     }
+// }
+
+// export const submitNewOrder = (formData) => {
+//     console.log(formData, 'form data in zction')
+//     return (dispatch) => {
+//         return axios
+//           .post('https://frosty-wood-6558.getsandbox.com/dishes', {
+//             headers: {
+//                 'Content-Type': 'application/json'
+//             },
+//           })
+//           .then((res) => {
+//             // dispatch(fetchFilmsAction(res.data));
+//             console.log(res)
+//           })
+//           .catch((err) => {
+//             console.log(err);
+//           });
+//       };
+// }
